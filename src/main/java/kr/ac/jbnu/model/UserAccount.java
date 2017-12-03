bac6481ceb5e9818586d7637e26fc487f396e2ae
dohun94 <dohun94@naver.com>
2017-12-03 14:57:33 +0900

    [추가] 나머지 모델과 하이버네이트 연동파일 추가

---
+++ src/main/java/kr/ac/jbnu/model/UserAccount.java
@@ -0,0 +1,75 @@
+package kr.ac.jbnu.model;
+
+public class UserAccount {
+
+	private String userName;
+	private String password;
+
+	private String id;
+	private String major;
+	private String email;
+
+	//수정필요
+	private String isAdmin;
+	private String blocked;
+	
+	public UserAccount() {
+
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getMajor() {
+		return major;
+	}
+	
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(String isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public String getBlocked() {
+		return blocked;
+	}
+
+	public void setBlocked(String blocked) {
+		this.blocked = blocked;
+	}
+}
